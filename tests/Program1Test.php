6b0848e73e9bd9102f39cddb430b7191848dec68
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-25 21:51:06 +0200

    changed test program.

--- tests/Program1Test.php
+++ tests/Program1Test.php
@@ -6,31 +6,78 @@ use Lechimp\STG\Lang\Variable;
 use Lechimp\STG\Lang\Lambda;
 use Lechimp\STG\Lang\PrimOp;
 use Lechimp\STG\Lang\Literal;
+use Lechimp\STG\Lang\Application;
+use Lechimp\STG\Lang\Constructor;
+use Lechimp\STG\Lang\CaseExpr;
+use Lechimp\STG\Lang\AlgebraicAlternative;
 use Lechimp\STG\Compiler;
 
 class Program1Test extends PHPUnit_Framework_TestCase {
     public function test_program() {
+        /**
+         * Represents the following program
+         * main = \{} \u \{} -> swapAB a
+         * a = \{} \n \{} -> A
+         * swapAB = \{} \n \{a} -> 
+         *     case a of
+         *         A -> B
+         *         B -> A   
+         */
         $program = new Program(array
             ( new Binding
                 ( new Variable("main")
                 , new Lambda
                     ( array()
                     , array()
-                    , new PrimOp
-                        ( "+"
+                    , new Application 
+                        ( new Variable("swapAB")
                         , array
-                            ( new Literal(1)
-                            , new Literal(2)
+                            ( new Variable("a") 
                             )
                         )
                     , true
                     )
                 )
+            , new Binding
+                ( new Variable("a")
+                , new Lambda
+                    ( array()
+                    , array()
+                    , new Constructor("A", array())
+                    , true
+                    )
+                )
+            , new Binding
+                ( new Variable("swapAB")
+                , new Lambda
+                    ( array()
+                    , array(new Variable("a"))
+                    , new CaseExpr
+                        ( new Application
+                            ( new Variable("a")
+                            , array()
+                            )
+                        , array
+                            ( new AlgebraicAlternative
+                                ( "A"
+                                , new Constructor("B", array())
+                                )
+                            , new AlgebraicAlternative
+                                ( "B"
+                                , new Constructor("A", array())
+                                )
+                            )
+                        )
+                    , false
+                    )
+                )
             ));
         $compiler = new Compiler();
         $compiled = $compiler->compile($program, "TheMachine"); 
+        echo ("\n\n".$compiled["main.php"]."\n\n");
         eval($compiled["main.php"]);
         $machine = new TheMachine();
         $result = $machine->run();
+        $this->assertEquals(array("B"), $result);
     }
 }
