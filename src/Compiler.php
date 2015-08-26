8b39f28c5e48e46eba65d7835350cff0912cf4bf
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-26 21:33:55 +0200

    started to move compiler to new php generator logic

--- src/Compiler.php
+++ src/Compiler.php
@@ -7,7 +7,7 @@ namespace Lechimp\STG;
  * code.
  */
 class Compiler {
-    const STG_VAR_NAME = '$stg';
+    const STG_VAR_NAME = 'stg';
 
     /**
      * Compile a program to a bunch of PHP files using the STG to execute the
@@ -20,97 +20,104 @@ class Compiler {
     public function compile(Lang\Program $program, $stg_class_name) {
         assert(is_string($stg_class_name));
 
-        $stg = self::STG_VAR_NAME; 
+        // Constant used when rendering.
+        $rc = array
+            ( "ns" => "" // Namespace
+            , 'stg' => self::STG_VAR_NAME;  // variable name for stg
+            );
+
         $globals = array();
         $code = "use \\Lechimp\\STG\\STG;\n".
                 "use \\Lechimp\\STG\\CodeLabel;\n".
                 "use \\Lechimp\\STG\\STGClosure;\n".
                 "\n\n";
+
+        $classes = array(); 
         
         foreach($program->bindings() as $binding) {
             $var_name = $binding->variable()->name();
             $class_name = ucfirst($var_name)."Closure";
-            $code .= $this->compile_lambda($binding->lambda(), $class_name)."\n\n";
+
+            // Closures for global lambdas.
+            $classes[] = $this->compile_lambda($rc, $binding->lambda(), $class_name);
+
+            // Instantiation of globals for STG class.
             $globals[$var_name] = "new $class_name()";
         }
         
-        $globals_code = implode( "\n            , "
-                               , array_map(function($v, $k) {
-                                    return "\"$k\" => $v";
-                                 }, $globals, array_keys($globals))
-                               );
+        // Class for the final stg machine
+        $classes[] = g_class( $rc["ns"], $stg_class_name
+            , array() // no props
+            , array
+                ( g_public_method( "__construct", array(), array
+                    ( g_stmt(function($ind) { return
+                        "parent::__construct(".g_multiline_dict($ind, $globals_code).");"; })
+                    )
+                );
+        );
 
-        $code .= "\n";
-        $code .= <<<PHP
-class TheMachine extends STG {
-    public function __construct() {
-        parent::__construct(array
-            ( $globals_code
-            ));
-    }
-}
-PHP;
+        // Render all classes to a single file.
+        $code .= implode("\n\n", array_map(function(GClass $cl) {
+            return $cl->render(0);
+        }, $classes));
+
         return array("main.php" => $code);
     }
 
-    protected function compile_lambda(Lang\Lambda $lambda, $class_name) {
+    protected function compile_lambda(array $rc, Lang\Lambda $lambda, $class_name) {
         assert(is_string($class_name));
 
-        $stg = self::STG_VAR_NAME; 
-
         $num_args = count($lambda->arguments());
-        $argument_stack_check = "assert({$stg}->count_args() >= $num_args);";
-
-        $get_free_variables = "//TODO: get_free_variables NYI!";
-
-        $arguments = array();
-        foreach ($lambda->arguments() as $argument) {
-            $argname = '$'.$argument->name();
-            $arguments[] = "$argname = {$stg}->pop_arg();";
-        }
-        $get_arguments = implode("\n        ", $arguments);
 
         list($compiled_expression, $additional_methods)
-             = $this->compile_expression($lambda->expression());
+             = $this->compile_expression($rc, $lambda->expression());
 
-        return <<<PHP
-class $class_name extends STGClosure {
-    public function entry_code(STG $stg) {
-        $argument_stack_check
-        $get_free_variables
-        $get_arguments
-        $compiled_expression 
+        return g_class($rc["ns"], $class_name
+            , array
+                (
+                )
+            , array_merge(array
+                ( g_public_method("entry_code", g_stg_args(), array_merge(array 
+                    ( g_stmt("assert(\${$rc['stg']}->count_args() >= $num_args)")
+                    , g_stmt("//TODO: get free variable NYI!")
+                    )
+                    , array_map(function($argument) {
+                            return g_stg_pop_arg_to($rc["stg"], "_".$argument->name());
+                        }, $lambda->arguments())
+                    , $compiled_expression
+                    )
+                )
+                ,
+                $additional_methods
+                )
+            , "STGClosure"
+            );
     }
 
-    $additional_methods
-}
-PHP;
-    }
-
-    protected function compile_expression(Lang\Expression $expression) {
+    protected function compile_expression(array $rc, Lang\Expression $expression) {
         if ($expression instanceof Lang\Application) {
-            return $this->compile_application($expression);
+            return $this->compile_application($rc, $expression);
         }
         if ($expression instanceof Lang\Constructor) {
-            return $this->compile_constructor($expression);
+            return $this->compile_constructor($rc, $expression);
         }
         if ($expression instanceof Lang\CaseExpr) {
-            return $this->compile_case_expression($expression);
+            return $this->compile_case_expression($rc, $expression);
         }
         throw new \LogicException("Unknown expression '$expression'.");
     }
 
-    protected function compile_application(Lang\Application $application) {
-        $stg = self::STG_VAR_NAME;
-        $code = array();
-        foreach ($application->atoms() as $atom) {
-            $atom = $this->compile_atom($atom);
-            $code[] =  "{$stg}->push_arg($atom);";
-        }
+    protected function compile_application(array $rc, Lang\Application $application) {
         $var_name = $application->variable()->name();
-        $code[] = "return {$stg}->enter({$stg}->global_var(\"$var_name\"));";
-        $code = implode("\n        ", $code);
-        return array($code, "");
+        return array
+            ( array_merge(array_map(function($atom) {
+                    return g_stg_push_arg($rc["stg"], $this->compile_atom($rc, atom)); 
+                }, $application->atoms())
+                ,
+                g_stg_enter($rc["stg"], g_stg_global_var($rc["stg"], $var_name));
+                )
+            , array()
+            );
     }
 
     protected function compile_constructor(Lang\Constructor $constructor) {
@@ -193,5 +200,43 @@ PHP;
         }
         throw new \LogicException("Unknown atom '$atom'.");
     }
-
 } 
+
+function g_class($namespace, $name, $properties, $methods) {
+    return new Lechimp\Gen\GClass($namespace, $name, $properties, $methods);
+}
+
+function g_public_method($name, $arguments, $statements) {
+    return new Lechimp\Gen\GPublicMethod($name, $arguments, $statements);
+}
+
+function g_stg_args() {
+    return array(new Lechimp\Gen\GArgument("STG", "stg"));
+}
+
+function g_stmt($code) {
+    return new \Lechimp\Gen\GStatement($code);
+}
+
+function g_multiline_dict($ind, array $array) {
+    assert(is_int($ind));
+    return implode("\n$ind," , array_map(function($v, $k) {
+                return "\"$k\" => $v";
+           }, $array, array_keys($array)));
+}
+
+function g_stg_pop_arg_to($stg_name, $arg_name) {
+    return new Lechimp\Gen\GStatement("\$$arg_name = \${$stg_name}->pop_arg()");
+}
+
+function g_stg_push_arg($stg_name, $what) {
+    return new Lechimp\Gen\GStatement("\$$arg_name = \${$stg_name}->push_arg($what)");
+}
+
+function g_stg_ebter($stg_name, $where) {
+    return new Lechimp\Gen\GStatement("\${$stg_name}->enter($where)");
+}
+
+function g_stg_global_var($stg_name, $var_name) {
+    return "\${$stg_name}->global_var(\"$var_name\")";
+}
