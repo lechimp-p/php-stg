8c5272310019c156f930c31e46cd38fd59138fbb
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-26 20:21:59 +0200

    started to write tests for generator classes.

--- tests/GenTest.php
+++ tests/GenTest.php
@@ -1,6 +1,12 @@
 <?php
 
 use Lechimp\STG\Gen\GClass;
+use Lechimp\STG\Gen\GPrivateProperty;
+use Lechimp\STG\Gen\GProtectedProperty;
+use Lechimp\STG\Gen\GPublicProperty;
+use Lechimp\STG\Gen\GPrivateMethod;
+use Lechimp\STG\Gen\GProtectedMethod;
+use Lechimp\STG\Gen\GPublicMethod;
 
 class GenText extends PHPUnit_Framework_TestCase {
     protected function assertCodeEquals($left, $right) {
@@ -20,6 +26,36 @@ class GenText extends PHPUnit_Framework_TestCase {
         $generated = $gen->render(0);
         $expected = <<<'PHP'
 class Lechimp\\STG\\Test {
+}
+PHP;
+        $this->assertCodeEquals($generated, $expected);
+    }
+
+    public function test_filledClass() {
+        $gen = new GClass("Lechimp\\STG", "Test"
+            , array
+                ( new GPrivateProperty("foo")
+                , new GProtectedProperty("bar")
+                , new GPublicProperty("baz")
+                )
+            , array
+                ( new GPrivateMethod("get_bar", array(), array())
+                , new GProtectedMethod("get_foo", array(), array())
+                , new GPublicMethod("__construct", array(), array())
+                )
+            );
+        $generated = $gen->render(0);
+        $expected = <<<'PHP'
+class Lechimp\\STG\\Test {
+    private $foo;
+    protected $bar;
+    public $baz;
+    public function get_bar() {
+    } 
+    protected function get_foo() {
+    } 
+    public function __construct() {
+    }
 }
 PHP;
         $this->assertCodeEquals($generated, $expected);
