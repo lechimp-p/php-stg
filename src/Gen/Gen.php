2b804f7a0ca70ae61d7ed9cf1ebeb786d447e24a
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-27 18:52:14 +0200

    added Gen::indent

--- src/Gen/Gen.php
+++ src/Gen/Gen.php
@@ -17,6 +17,20 @@ abstract class Gen {
     abstract public function render($indentation);
 
     /**
+     * Indent a string.
+     *
+     * @param   int         $indentation
+     * @param   string      $string
+     * @return  string
+     */
+    public function indent($indentation, $string) {
+        assert(is_int($indentation));
+        assert(is_string($string));
+        return str_repeat(self::INDENTATION_ATOM, $indentation).$string;
+    }
+     
+
+    /**
      * Take multiple lines in an array and concatenate them
      * together with some indentation.
      *
@@ -25,7 +39,7 @@ abstract class Gen {
      * @return  string
     */
     protected function cat_and_indent($indentation, array $strings) {
-        $ind = str_repeat(self::INDENTATION_ATOM, $indentation);
+        $ind = $this->indent($indentation, "");
         return implode("\n", array_map(function($s) use ($ind) {
             return $ind.$s;
         }, $strings));
