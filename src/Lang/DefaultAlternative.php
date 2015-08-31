69f354eb990ac4c99d264ba8978f66f23abcaa00
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-31 14:18:32 +0200

    fixed default alternative, no need for a variable

--- src/Lang/DefaultAlternative.php
+++ src/Lang/DefaultAlternative.php
@@ -3,17 +3,7 @@
 namespace Lechimp\STG\Lang;
 
 class DefaultAlternative extends Alternative {
-    /**
-     * @var Variable|null
-     */
-    private $variable;
-
-    public function __construct(Variable $variable, Expression $expression) {
-        $this->variable = $variable;
+    public function __construct(Expression $expression) {
         $this->expression = $expression;
-    }
-
-    public function variable() {
-        return $this->variable;
     }
 }
