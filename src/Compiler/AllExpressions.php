e2455e8432e55055235ea6f061258eaef0b0ffe5
Richard Klees <richard.klees@rwth-aachen.de>
2015-10-18 13:14:43 +0200

    added missing files.

---
+++ src/Compiler/AllExpressions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lechimp\STG\Compiler;
+
+use Lechimp\STG\Lang;
+use Lechimp\STG\Gen\Gen;
+
+class AllExpressions extends Pattern {
+    /**
+     * @inheritdoc
+     */
+    public function matches(Lang\Syntax $c) {
+        if ($c instanceof Lang\Expression) {
+            return true;
+        } 
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function compile(Compiler $c, Gen $g, &$_) {
+        throw new \LogicException("Can't use pattern AllExpressions to compile something.");
+    } 
+}
+
