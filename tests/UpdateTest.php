e839ad54bfd31f6c06ae9d44d1aadcccb6165178
Richard Klees <richard.klees@rwth-aachen.de>
2021-06-19 22:03:03 +0200

    simplify programm notation in UpdateTest

--- tests/UpdateTest.php
+++ tests/UpdateTest.php
@@ -23,105 +23,58 @@ class UpdateTest extends ProgramTestBase
          *          V v1 -> case b of
          *              V v2 -> Result v1 v2
          */
-        $program = $l->program(array( $l->binding(
-            $l->variable("main"),
-            $l->lambda(
-                array(),
-                array(),
-                $l->letrec(
-                    array( $l->binding(
-                        $l->variable("v"),
-                        $l->lambda(
-                            array(),
-                            array($l->variable("c")),
-                            $l->constructor(
+        $program = $l->prg([
+            "main" => $l->lam_n(
+                $l->ltr(
+                    [
+                        "v" => $l->lam_a(
+                            ["c"],
+                            $l->con(
                                 "V",
-                                array( $l->variable("c")
-                                        )
+                                "c"
                             ),
                             false
-                        )
-                    )
-                        , $l->binding(
-                            $l->variable("a"),
-                            $l->lambda(
-                                array($l->variable("v")),
-                                array(),
-                                $l->application(
-                                    $l->variable("v"),
-                                    array($l->literal(42))
-                                ),
-                                true
-                            )
-                        )
-                        , $l->binding(
-                            $l->variable("t"),
-                            $l->lambda(
-                                array($l->variable("a")),
-                                array(),
-                                $l->constructor(
-                                    "T",
-                                    array( $l->variable("a")
-                                        , $l->variable("a")
-                                        )
-                                ),
-                                true
-                            )
-                        )
-                        )
-            /*  This part is:
-             *  in case t of
-             *      T a b -> case a of
-             *          V v1 -> case b of
-             *              V v2 -> Result v1 v2
-             */
-                        ,
-                    $l->case_expr(
-                        $l->application(
-                            $l->variable("t"),
-                            array()
                         ),
-                        array( $l->algebraic_alternative(
-                            "T",
-                            array( $l->variable("a")
-                                        , $l->variable("b")
-                                        ),
-                            $l->case_expr(
-                                $l->application(
-                                    $l->variable("a"),
-                                    array()
-                                ),
-                                array( $l->algebraic_alternative(
-                                    "V",
-                                    array($l->variable("v1")),
-                                    $l->case_expr(
-                                        $l->application(
-                                            $l->variable("b"),
-                                            array()
-                                        ),
-                                        array( $l->algebraic_alternative(
-                                            "V",
-                                            array($l->variable("v2")),
-                                            $l->constructor(
-                                                "Result",
-                                                array( $l->variable("v1")
-                                                                    , $l->variable("v2")
-                                                                    )
-                                            )
-                                        )
-                                                        )
-                                    )
-                                )
-                                            )
+                        "a" => $l->lam_f(
+                            ["v"],
+                            $l->app(
+                                "v",
+                                $l->lit(42)
+                            )
+                        ),
+                        "t" => $l->lam_f(
+                            ["a"],
+                            $l->con(
+                                "T",
+                                "a",
+                                "a"
                             )
                         )
-                                )
+                    ],
+                    $l->cse(
+                        $l->app("t"),
+                        [
+                            "T a b" => $l->cse(
+                                $l->app("a"),
+                                [
+                                    "V v1" => $l->cse(
+                                        $l->app("b"),
+                                        [
+                                            "V v2" => $l->con(
+                                                "Result",
+                                                "v1",
+                                                "v2"
+                                            )
+                                        ]
+                                    )
+                                ]
+                            )
+                        ]
                     )
-                ),
-                false
+                )
             )
-        )
-            ));
+        ]);
+
         $compiler = new Compiler();
         $compiled = $compiler->compile($program, "TheMachine", "UpdateTest");
         //$this->echo_program($compiled["main.php"]);
